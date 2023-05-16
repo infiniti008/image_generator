492bc3c619d91f8a653e6c9ea3af213b2c1f9bb9
infiniti008 <nikitenok.sl@gmail.com>
2023-05-16 17:24:14 +0200

    - Add no sandbox

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.get(`/api/currency/render`, async function(req, res) {
   
   const html = req.query.html;
   const image = await nodeHtmlToImage({
-    html: html
+    html: html,
+    puppeteerArgs: {
+      args: ['--no-sandbox']
+    }
   });
   console.log('IMAGE GENERATED');
   res.writeHead(200, { 'Content-Type': 'image/png' });
