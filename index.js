6ce36ca8de6c26b71525ae17ebffdac52d3c355b
infiniti008 <nikitenok.sl@gmail.com>
2023-05-16 16:57:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const nodeHtmlToImage = require('node-html-to-image');
+
+const app = express();
+
+app.listen(5100);
+
+app.get(`/api/currency/render`, async function(req, res) {
+  
+  const html = req.query.html;
+  const image = await nodeHtmlToImage({
+    html: html
+  });
+  console.log('IMAGE GENERATED');
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+  res.end(image, 'binary');
+});
