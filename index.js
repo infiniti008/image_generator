4aa56006e2b78f736cf79d3cb715a38f3d274bd1
infiniti008 <nikitenok.sl@gmail.com>
2023-05-17 18:23:52 +0200

    - Add error handler

--- index.js
+++ index.js
@@ -8,29 +8,33 @@ app.use(express.json());
 app.listen(5100);
 
 app.post('/api/render', async function(req, res) {
-  console.log('START GENERATING');
-  const { html, type = 'png', quality, content, encoding = 'binary', selector, puppeteerArgs = [] } = req.body.data;
-  
-  const image = await nodeHtmlToImage({
-    html,
-    type,
-    quality,
-    content,
-    encoding,
-    selector,
-    puppeteerArgs: {
-      args: ['--no-sandbox', ...puppeteerArgs]
-    }
-  });
+  try {
+    console.log('START GENERATING');
+    const { html, type = 'png', quality, content, encoding = 'binary', selector, puppeteerArgs = [] } = req.body.data;
+    
+    const image = await nodeHtmlToImage({
+      html,
+      type,
+      quality,
+      content,
+      encoding,
+      selector,
+      puppeteerArgs: {
+        args: ['--no-sandbox', ...puppeteerArgs]
+      }
+    });
 
-  console.log('IMAGE GENERATED');
-  
-  if (encoding === 'binary') {
-    console.log('SENT BINARY IMAGE');
-    res.writeHead(200, { 'Content-Type': 'image/' + type });
-    res.end(image, encoding);
-  } else {
-    console.log('SENT BASE64 IMAGE');
-    res.send(image);
+    console.log('IMAGE GENERATED');
+    
+    if (encoding === 'binary') {
+      console.log('SENT BINARY IMAGE');
+      res.writeHead(200, { 'Content-Type': 'image/' + type });
+      res.end(image, encoding);
+    } else {
+      console.log('SENT BASE64 IMAGE');
+      res.send(image);
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
