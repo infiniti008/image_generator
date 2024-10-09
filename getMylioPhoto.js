aafb65ce73d75ad6c3fb3aa0636d8929ce16b7d3
infiniti008 <nikitenok.sl@gmail.com>
2024-10-09 12:01:21 +0200

    Disable mylio

--- getMylioPhoto.js
+++ getMylioPhoto.js
@@ -1,41 +1,41 @@
-// open browser. then open page and then find tags a and get all src from them
-import * as dotenv from 'dotenv';
-dotenv.config({
-  path: './.env'
-});
+// // open browser. then open page and then find tags a and get all src from them
+// import * as dotenv from 'dotenv';
+// dotenv.config({
+//   path: './.env'
+// });
 
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
-const env = process.env.environment || 'prod';
-const executablePath = process.env['executablePath_' + env];
+// const env = process.env.environment || 'prod';
+// const executablePath = process.env['executablePath_' + env];
 
-export const getMylioPhoto = async (url) => {
-  // Launch the browser
-  const browser = await puppeteer.launch({
-    executablePath,
-    headless: true,
-    slowMo: 10,
-    args: ['--no-sandbox', '--lang=en-US'],
-    ignoreHTTPSErrors: true
-  });
-  const page = await browser.newPage();
+// export const getMylioPhoto = async (url) => {
+//   // Launch the browser
+//   const browser = await puppeteer.launch({
+//     executablePath,
+//     headless: true,
+//     slowMo: 10,
+//     args: ['--no-sandbox', '--lang=en-US'],
+//     ignoreHTTPSErrors: true
+//   });
+//   const page = await browser.newPage();
 
-  // Open the page
-  await page.goto(url); // Replace with your URL
+//   // Open the page
+//   await page.goto(url); // Replace with your URL
 
-  await page.waitForSelector('a.imageListItem img');
+//   await page.waitForSelector('a.imageListItem img');
 
-  // Find all <a> tags and get their href attributes
-  const links = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll('a.imageListItem img')).map(img => img.src);
-  });
+//   // Find all <a> tags and get their href attributes
+//   const links = await page.evaluate(() => {
+//     return Array.from(document.querySelectorAll('a.imageListItem img')).map(img => img.src);
+//   });
 
-  // console.log(links);
+//   // console.log(links);
 
-  // Close the browser
-  await browser.close();
+//   // Close the browser
+//   await browser.close();
 
-  return links;
-}
+//   return links;
+// }
 
-// getMylioPhoto('https://share.mylio.com/769334aebc651f62be29558561af723fb8da580c/422cfe8e0f423a839a45fce12493686f10addcd8/');
+// // getMylioPhoto('https://share.mylio.com/769334aebc651f62be29558561af723fb8da580c/422cfe8e0f423a839a45fce12493686f10addcd8/');
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { postToInstagramReels } from './postToInstagramPost.js';
 import { postToTikTok } from './postToTikTok.js';
 import { render } from './renderImage.js';
 import { renderVideo } from './renderVideo.js';
-import { getMylioPhoto } from './getMylioPhoto.js';
+// import { getMylioPhoto } from './getMylioPhoto.js';
 import Browser from './browser.js';
 
 const browser = new Browser();
@@ -122,16 +122,16 @@ app.post('/api/render/image', async function(req, res) {
   }
 });
 
-app.get('/api/get-mylio-photo', async function(req, res) {
-  try {
-    const { url } = req.query;
-    const links = await getMylioPhoto(url);
-    res.json(links);
-  } catch (err) {
-    console.log(err);
-    res.json([]);
-  }
-});
+// app.get('/api/get-mylio-photo', async function(req, res) {
+//   try {
+//     const { url } = req.query;
+//     const links = await getMylioPhoto(url);
+//     res.json(links);
+//   } catch (err) {
+//     console.log(err);
+//     res.json([]);
+//   }
+// });
 
 function getTimezone(subscription) {
   if (subscription.country === 'by') {
